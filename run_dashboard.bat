2d9716bdde71f60a968290587d4e70aaa867a972
Klimmgo <132894770+Klimmgo@users.noreply.github.com>
2024-06-17 12:46:02 +0200

    Update run_dashboard.bat

--- run_dashboard.bat
+++ run_dashboard.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:\Users\Installation\AppData\Local\Programs\Python\Python312\python.exe" "D:\Studium\2. Semester\02_Funktionale Programmierung mit Python\1.1.3 Finalisierungsphase\Python\objektorientiert\main.py"
+"your python directory\python.exe" "your installation directory\main.py"
 pause
